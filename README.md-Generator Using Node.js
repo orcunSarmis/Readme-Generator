23527aaf46317bfdc9df9a52974527a53a3374a9
orcunSarmis <79064464+orcunSarmis@users.noreply.github.com>
2022-05-12 18:00:11 +1000

    Create README.md-Generator Using Node.js
    Main README.md to use the application.

---
+++ README.md-Generator Using Node.js
@@ -0,0 +1,51 @@
+#README.md Generator Using Node.js
+
+![GitHub license](https://img.shields.io/badge/license-APACHE2.0-blue.svg)
+
+##Description
+
+The README.md generator is a node.js application that uses user input from inquirer to populate a README.md file for user repository.
+
+##Table of Content
+
+* Usage
+* Contributing
+* Installation
+* Tests
+* Licence
+* Questions
+
+##Usage
+
+Clone the repo first and run the following command at the root of your project and answer the prompted questions:
+
+```
+npm i
+
+```
+the application will then generate a README.md file.
+                  
+##Contributing
+
+Ideas to improve this application are welcome: , email me please.
+
+##Installation
+ 
+To install necessary dependencies, run the following command:
+  ```
+  npm i
+  ```
+##Tests
+
+To run tests, run the following command:
+  ```
+  npm test
+  ```
+  
+ ##License
+
+   This project is licensed under the [APACHE 2.0](https://www.apache.org/licenses/LICENSE-2.0) license. 
+
+ ##Questions
+
+  If you have any questions about the repo, open an issue or contact me directly at yorcunsarmis@gmail.com. You can find more of my work at [orcunSarmis](https://github.com/orcunSarmis/).
